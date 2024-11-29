a674173ff82a7d3a46734912fb1b8cfba374753e
solomon <ssbothwell@gmail.com>
2024-11-29 12:21:24 -0800

    Use a proxy to constrain event types when parsing

--- src/API.hs
+++ src/API.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE ImportQualifiedPost #-}
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
@@ -14,9 +15,9 @@ where
 
 --------------------------------------------------------------------------------
 
-import Data.Aeson ((.:))
 import Data.Aeson qualified as Aeson
 import Data.Proxy (Proxy (..))
+import GHC.Generics
 import Servant ((:<|>) (..), (:>))
 import Servant qualified
 
@@ -28,28 +29,24 @@ type Song = String
 
 type Duration = Int
 
-data Event = WakeUp | Eat Meal | RockOut Song Duration
-  deriving stock Show
+data PayloadWakeUp = PayloadWakeUp
+  deriving stock (Show, Generic)
 
-instance Aeson.FromJSON Event where
-  parseJSON = Aeson.withObject "Event" $ \ob -> do
-      event :: String <- ob .: "event"
-      case event of
-        "wakeUp" -> parseWakeUp
-        "eat" -> parseEat ob 
-        "rockOut" -> parseRockOut ob
-        _ -> fail "Invalid Event"
-    where
-      parseWakeUp = pure WakeUp
+newtype PayloadEat = PayloadEat { meal :: Meal }
+  deriving stock (Show, Generic)
 
-      parseEat ob = do
-        meal <- ob .: "meal"
-        pure (Eat meal)
+data PayloadRockOut = PayloadRockOut { song :: Song, duration :: Duration }
+  deriving stock (Show, Generic)
 
-      parseRockOut ob = do
-        song <- ob .: "song"
-        duration <- ob .: "duration"
-        pure (RockOut song duration)
+instance Aeson.FromJSON PayloadWakeUp
+instance Aeson.FromJSON PayloadEat
+instance Aeson.FromJSON PayloadRockOut
+
+data Event =
+   EventWakeUp PayloadWakeUp
+ | EventEat PayloadEat
+ | EventRockOut PayloadRockOut
+  deriving Show
 
 --------------------------------------------------------------------------------
 
@@ -63,20 +60,20 @@ type EventAPI =
           :<|> "rock-out" :> Req :> Resp
              )
 
-importEvent :: Aeson.Value -> Servant.Handler String
-importEvent blob =
-  case Aeson.fromJSON @Event blob of
+importEvent :: forall e. (Aeson.FromJSON e, Show e) => Proxy e -> Aeson.Value -> Servant.Handler String
+importEvent _ blob =
+  case Aeson.fromJSON @e blob of
     Aeson.Error err -> fail err
     Aeson.Success res -> pure (show res)
 
 wakeUp :: Aeson.Value -> Servant.Handler String
-wakeUp = importEvent
+wakeUp = importEvent (Proxy @PayloadWakeUp)
 
 eat :: Aeson.Value -> Servant.Handler String
-eat = importEvent
+eat = importEvent (Proxy @PayloadEat)
 
 rockOut :: Aeson.Value -> Servant.Handler String
-rockOut = importEvent
+rockOut = importEvent (Proxy @PayloadRockOut)
 
 eventServer :: Servant.Application
 eventServer = Servant.serve (Proxy @EventAPI) $ wakeUp :<|> eat :<|> rockOut
